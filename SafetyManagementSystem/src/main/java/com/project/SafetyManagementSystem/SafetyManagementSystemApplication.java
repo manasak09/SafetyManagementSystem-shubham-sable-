313d8c3b2c602ac627aec295ea635e563935d1e6
Group6 <103427156+manasak09@users.noreply.github.com>
2022-04-12 10:41:53 +0530

    Update SafetyManagementSystemApplication.java

--- SafetyManagementSystem/src/main/java/com/project/SafetyManagementSystem/SafetyManagementSystemApplication.java
+++ SafetyManagementSystem/src/main/java/com/project/SafetyManagementSystem/SafetyManagementSystemApplication.java
@@ -2,8 +2,13 @@ package com.project.SafetyManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication (scanBasePackages= {"com.model","com.dao"})
+
+@EntityScan("com.model")
+@EnableJpaRepositories({"com.dao"})
 public class SafetyManagementSystemApplication {
 
 	public static void main(String[] args) {
